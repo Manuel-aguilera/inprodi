e9d6ab8f8970b308cb57dd2b9cef32b7ce00e30b
Manuel-aguilera <Manueleitor.information@gmail.com>
2020-10-17 15:40:58 -0500

    1.0 Page Login.js

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,48 @@
 import React from 'react';
+import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import '../assets/styles/components/Form.scss';
+import logo from '../assets/static/logo.png';
 
-const Form = () => {
-  return <h1>Hola</h1>;
-};
+const Forma = () => (
+  <div className='container_form'>
+    <img className='logo_form' src={logo} alt='logo' />
+    <Form>
+      <FormText className='title_form'>Iniciar Sesión</FormText>
+      <FormGroup className='email_form'>
+        <Label for='email'>Correo electrónico</Label>
+        <Input
+          type='email'
+          name='email'
+          id='email'
+          placeholder='carlosmrtz@gmail.com'
+        />
+      </FormGroup>
+      <FormGroup className='password_form'>
+        <Label for='password'>Contraseña</Label>
+        <Input
+          type='password'
+          name='password'
+          id='password'
+          placeholder='*******************'
+        />
+      </FormGroup>
 
-export default Form;
+      <div className='footer_form'>
+        <Button className='submit_form'>INICIAR SESIÓN</Button>
+
+        <FormGroup className='signup_form'>
+          <Label for='registrate'>¿Aún no tienes cuenta?</Label>
+          <Button id='registrate' color='link'>
+            Regístrate
+          </Button>
+        </FormGroup>
+
+        <Button className='forgot_form' id='forgot' color='link'>
+          ¿Olvidaste la contraseña?
+        </Button>
+      </div>
+    </Form>
+  </div>
+);
+
+export default Forma;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import '../assets/styles/Login.scss';
+import Form from '../components/Form';
+import login from '../assets/static/login.png';
 
 const Login = () => {
   return (
-    <h1>Hola mundo</h1>
+    <div className='container_login'>
+      <Form className='my_form' />
+      <img src={login} alt='login-img' />
+    </div>
   );
 };
 
