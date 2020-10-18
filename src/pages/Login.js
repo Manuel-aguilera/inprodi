8136631055647ad73e67cf0bbd4a86a7113b8f54
Manuel-aguilera <Manueleitor.information@gmail.com>
2020-10-18 15:24:51 -0500

    2.0 Page Login.js terminate

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,13 +1,29 @@
 import React from 'react';
-import '../assets/styles/Login.scss';
 import Form from '../components/Form';
 import login from '../assets/static/login.png';
 
 const Login = () => {
   return (
-    <div className='container_login'>
-      <Form className='my_form' />
-      <img src={login} alt='login-img' />
+    <div className='container '>
+      <div className='row align-items-center'>
+        <div className='login_form col-sm-12 col-md-7 col-lg-6 col-xl-6 align-self-center imagenFondo'>
+          <Form className='my_form' />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+        </div>
+        <div className='login_img col-sm-12 col-md-5 col-lg-6 col-xl-6 align-self-center'>
+          <img src={login} alt='login-img' />
+        </div>
+      </div>
     </div>
   );
 };
